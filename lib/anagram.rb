01ba62aa76e5b5fe4a97608fb1b67eca056a337c
Jason Viger <jason.viger@gmail.com>
2018-08-19 07:38:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram_array)
+    anagrams = []
+    anagram_array.each do |x|
+      if x == word.reverse
+        anagrams.push(x)
+      end
+    anagrams
+  end
+  
+  
+end
